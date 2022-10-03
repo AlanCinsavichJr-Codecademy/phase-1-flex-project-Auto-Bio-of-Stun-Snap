8a19cf071174ee95ab488b68dd8f75e88f028bf4
Alan Cinsavich <alan.cimsavich@gmail.com>
2022-10-03 18:43:23 -0400

    13 commit update HTML and JS

--- index.js
+++ index.js
@@ -1,18 +1,36 @@
 let newIDNum = 0
 
-const key = "";
+const ket = "26847882-ce742f707a7c77e680249c5b0"
 
-
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded",()=>{
     document.getElementById("Photo-Gallery-of-Star-Cars-button").addEventListener("onlcick",e=>{
         e.preventDefault();
-        document.getElementById("Photo-Gallery-of-Star-Cars").innerHTML = '';
-    }));
-    document.getElementById("Hot-Models-Photo-Gallery-button").addEventListener("onclick"), e=>{
-        e.preventDefault();
-        document.getElementById("Hot-Models-Photo-Gallery").innerHTML = '';
-
+        document.getElementById("Photo-Gallery-of-Star-Cars").innerHTML ='';
+        getPixaStarCarsPhotos(e.target.topic.value);
     });
 
+
+
+    document.getElementById("Hot-Models-Photo-Gallery-button").addEventListener("onclick",e=>{
+        e.preventDefault();
+        document.getElementById("Hot-Models-Photo-Gallery").innerHTML = '';
+        getPixaHotModelsPhotos(e.target.topic.value)
+    });
+
+getPixaStarCarsPhotos("");
+getPixaHotModelsPhotos("");
+
 });
+
+
+
+function getPixaStarCarsPhotos(topic) {
+    document.getElementById("Star-Cars-Photo-Gallery").style.display = "flex";
+    fetch(`https://pixabay.com/api/?key=${key}&q=${topic}&image_type=photo`).then(r=>r.json()).then(j=>j.hits.forEach(renderPhotoPost));
+}
+
+function getPixaHotModelsPhotos(topic) {
+    document.getElementById("Hot-Models-Cosplay-Gallery").style.display = "flex";
+    fetch(`https://pixabay.com/api/?key=${key}&q=${topic}&image_type=photo`).then(r=>r.json()).then(j=>j.hits.forEach(renderPhotoPost));
+}
 
