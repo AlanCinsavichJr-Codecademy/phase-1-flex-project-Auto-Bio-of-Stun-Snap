56d57d41bbad3284dec6e6e0f5a8241f6b3bef16
Alan Cinsavich <alan.cimsavich@gmail.com>
2022-10-05 17:16:43 -0400

    16th Commit First Code Draft Ready

--- index.js
+++ index.js
@@ -12,7 +12,11 @@ document.addEventListener("DOMContentLoaded",()=>{
     });
 
     document.getElementById("Like-Button").addEventListener("click",()=>{
-        
+        document.getElementById("Introduction").style.display = "none";
+        document.getElementById("Like-Button").style.display = "none";
+        document.getElementById("Vacations-and-Models").style.display = "none";
+        document.getElementById("Subscribe").style.display = "none";
+        document.getElementById("container").style.display = "block";
     })
 
     document.getElementById("Hot-Models-Photo-Gallery-button").addEventListener("onclick",e=>{
@@ -27,11 +31,28 @@ getPixaHotModelsPhotos("");
 });
 
 
-
-
 function getPixaStarCarsPhotos(topic) {
     document.getElementById("Star-Cars-Photo-Gallery").style.display = "flex";
     fetch(`https://pixabay.com/api/?key=${key}&q=${topic}&image_type=photo`).then(r=>r.json()).then(j=>j.hits.forEach(renderPhotoPost));
+}
+
+function renderPhotoPostOne(photoData) {
+
+    let photoPost = document.createElement("div");
+    photoPost.id = `post${photoData.id}`;
+    photoPost.className = "photo-post";
+
+    let photo = document.createElement("img");
+    photo.src = photoData.webformatURL;
+
+    let linkElement = document.createElement("a");
+    linkElement.href = photoData.pageURL;
+    linkElement.target = "_blank";
+    linkElement.textContent = `Photo by ${photoData.user}`;
+
+    photoPost.append(photo);
+    document.getElementById("Star-Cars-Photo-Gallery").append(photoPost,linkElement);
+
 }
 
 button.addEventListener('click', () => {
@@ -43,7 +64,38 @@ function getPixaHotModelsPhotos(topic) {
     fetch(`https://pixabay.com/api/?key=${key}&q=${topic}&image_type=photo`).then(r=>r.json()).then(j=>j.hits.forEach(renderPhotoPost));
 }
 
+function renderPhotoPostTwo(photoData) {
+
+    let photoPost = document.createElement("div");
+    photoPost.id = `post${photoData.id}`;
+    photoPost.className = "photo-post";
+
+    let photo = document.createElement("img");
+    photo.src = photoData.webformatURL;
+
+    let linkElement = document.createElement("a");
+    linkElement.href = photoData.pageURL;
+    linkElement.target = "_blank";
+    linkElement.textContent = `Photo by ${photoData.user}`;
+
+    photoPost.append(photo);
+    document.getElementById("Star-Cars-Photo-Gallery").append(photoPost,linkElement);
+
+}
+
 button.addEventListener('click', () => {
     button.classList.toggle('liked')
 })
 
+function createPhotoObj(e) {
+    newIDNum++;
+    let photo = {
+        id: newIDNum,
+        webformatURL: e.target.webformatURL.value,
+        webformatWidth: e.target.webformatWidth.value,
+        webformatHeight: e.target.webformatHeight.value,
+        user: e.target.user.value,
+        likes: 0
+    };
+    return photo;
+}
