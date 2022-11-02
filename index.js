9883ad9c6df3efe7b1aeded7455c930aa56b0042
Alan Cinsavich <alan.cimsavich@gmail.com>
2022-11-02 17:53:12 -0400

    37th commit

--- index.js
+++ index.js
@@ -4,30 +4,32 @@ const key = "31025559-1d12f38f3606df5b59db8ee14"
 const button = document.querySelector('.btn')
 const carGallery= document.getElementById("Photo Gallery of Star Cars");
 const modelGallery = document.getElementById("Hot Models Photo Gallery"); 
+const topic = ("Hot+Rod+Cars")
+
+console.log("This part works")
+
+function getPixaPhotos(carGallery){
+    fetch(`https://pixabay.com/api/?${key}&q=${topic}&image_type=photo`)
+    .then(r => r.json())
+    .then(carGallery.forEach(renderPhotoPost(topic)))
+    
+
+getPixaPhotos(carGallery);
+//topic is invoked wthe getPixaPhotos above along with location
+
+console.log("WHY WHY")
+    
+} 
 
 document.addEventListener("DOMContentLoaded",()=>{
-    return carGallery, modelGallery;
+    // return carGallery, modelGallery;
    });
-
-const topic1 = "https://pixabay.com/images/search/hot%20rod%20cars/?manual_search=1"
-const topic2 = "https://pixabay.com/images/search/female%20swimsuit%20models/?manual_search=1"
-
-
-function getPixaPhotos(topic1,topic2){
-    fetch(`https://pixabay.com/api/?key=${key}&q=${topic1,topic2}&image_type=photo`)
-    .then(r => r.json())
-    .then(j => j.hits.forEach(renderPhotoPost,carGallery,modelGallery))
-    // .then(j => j.hits.forEach(renderPhotoPost, modelGallery))
-}    
 
     // document.getElementById("Like-Button").addEventListener("click",e=>{
     //     e.preventDefault();
     //     // Add Like counter here
 
     // })
-
-getPixaPhotos(carGallery,modelGallery);
-//topic is invoked wthe getPixaPhotos above along with location
 
 function renderPhotoPost(photoData) {
 
